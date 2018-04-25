8789fa5b14cb88d1f5a572667a5e3fe108b7b8df
Olushola Karokatose <shola.karo@gmail.com>
2018-04-25 16:05:51 +0100

    Simple Express Server

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+ '/public'));
+
+app.use((req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<html><body><h1>This is an express server.</h1></body></html>');
+});
+
+const server = http.createServer(app); 
+server.listen(port, hostname, () => {
+	console.log(`Server listening on http://${hostname}:${port}/`);
+})
